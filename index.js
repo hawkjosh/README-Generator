68a5ed07bc67da4bd8a55eeadb01e589f6f8d45a
Josh Hawk <hawkjosh@icloud.com>
2022-07-25 10:00:11 -0400

    Content update push

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
+// Packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-inquirer
-  .prompt([
-    // Pass the questions in...
+// Array of questions for user input
+const questions = [
     {
       type: 'input',
       message: 'What is your GitHub username?',
-      name: 'github',
+      name: 'github'
     },
     {
-      type: 'XXXXX',  // Type of prompt (written as a string) i.e. → input, number, confirm, list, rawlist, expand, checkbox, password, editor
-      message: 'XXXXX',  // The prompt to be displayed (written as a string)
-      name: 'XXXXX',  // The name to use when storing the answer (written as a string)
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email'
     },
     {
-      type: 'XXXXX',  // Type of prompt (written as a string) i.e. → input, number, confirm, list, rawlist, expand, checkbox, password, editor
-      message: 'XXXXX',  // The prompt to be displayed (written as a string)
-      name: 'XXXXX',  // The name to use when storing the answer (written as a string)
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title'
     },
     {
-      type: 'XXXXX',  // Type of prompt (written as a string) i.e. → input, number, confirm, list, rawlist, expand, checkbox, password, editor
-      message: 'XXXXX',  // The prompt to be displayed (written as a string)
-      name: 'XXXXX',  // The name to use when storing the answer (written as a string)
+      type: 'input',
+      message: 'Provide a brief description that explains your project to the user.',
+      name: 'description'
     },
     {
-      type: 'XXXXX',  // Type of prompt (written as a string) i.e. → input, number, confirm, list, rawlist, expand, checkbox, password, editor
-      message: 'XXXXX',  // The prompt to be displayed (written as a string)
-      name: 'XXXXX',  // The name to use when storing the answer (written as a string)
+      type: 'input',
+      message: 'How will the user install your project and get the development environment running?',
+      name: 'install'
     },
     {
-      type: 'XXXXX',  // Type of prompt (written as a string) i.e. → input, number, confirm, list, rawlist, expand, checkbox, password, editor
-      message: 'XXXXX',  // The prompt to be displayed (written as a string)
-      name: 'XXXXX',  // The name to use when storing the answer (written as a string)
+      type: 'input',
+      message: 'Provide instructions for the user to implement your project.',
+      name: 'usage'
     },
     {
-      type: 'XXXXX',  // Type of prompt (written as a string) i.e. → input, number, confirm, list, rawlist, expand, checkbox, password, editor
-      message: 'XXXXX',  // The prompt to be displayed (written as a string)
-      name: 'XXXXX',  // The name to use when storing the answer (written as a string)
-    },
-    {
-      type: 'XXXXX',  // Type of prompt (written as a string) i.e. → input, number, confirm, list, rawlist, expand, checkbox, password, editor
-      message: 'XXXXX',  // The prompt to be displayed (written as a string)
-      name: 'XXXXX',  // The name to use when storing the answer (written as a string)
-    },
-  ])
-  .then((answers) => {
-    // Use user feedback for...
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+      type: 'rawlist',
+      message: 'What type of license should this project have?',
+      choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'None'],
+      name: 'license'
     }
-  });
+];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+// function init() {}
+
+// Function call to initialize app
+// init();
